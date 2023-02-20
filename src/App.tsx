7117a1b6efcb024e5d83a1e73261e57cc473202e
julien-go <juliengodard97@gmail.com>
2023-02-20 11:35:46 +0100

    handle status & delete todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import { AddTodo } from './AddTodo'
 import { List } from './List'
 import type { TodoType, TodoListType } from './@types/todo'
@@ -10,11 +10,30 @@ function App() {
   const addTodo = (newTodo: string) => {
     setTodos([...todos, {task: newTodo, status: false}])
   }
+  const removeTodo = (id: number) => {
+    setTodos(todos.filter((element, index) => index !== id))
+  }
+
+  const handleStatus = (id: number) => {
+    const newArray = todos.map((element, i) => {
+      if(id === i){
+        return {...element, status: !element.status}
+      } else {
+        return element
+      }
+    })
+    setTodos(newArray)
+  }
+
+  useEffect(()=> {
+    console.log(todos)
+  })
   
   return (
         <div className="App">
-          <AddTodo addTodo={addTodo}></AddTodo>
-          <List todos={todos}></List>
+          <h1>Todolist React Typescript</h1>
+          <AddTodo addTodo={addTodo}/>
+          <List todos={todos} handleStatus={handleStatus} removeTodo={removeTodo}/>
         </div>
     
   )
--- src/List.tsx
+++ src/List.tsx
@@ -2,16 +2,26 @@ import { useState } from 'react'
 import type { TodoType, TodoListType } from './@types/todo'
 
 type Props = {
-	todos: TodoListType
+	todos: TodoListType,
+	handleStatus: (id: number) => void,
+	removeTodo: (id: number) => void
 }
 
-export const List =({todos}: Props)=> {
+export const List =({todos, handleStatus, removeTodo}: Props)=> {
+
+	const checkUncheck= (id: number) => {
+		handleStatus(id)
+	}
 
 	return (
 		<div>
 			{todos.map((todo, id) => {
 				return (
-					<p>{todo.task}</p>
+					<div key={id} style={{border: 'black 1px solid'}}>
+						<p>{todo.task}</p>
+						<input type='checkbox' checked={todo.status} onChange={() => checkUncheck(id)}/>
+						<button onClick={() => removeTodo(id)}>Supprimer</button>
+					</div>
 				)
 			})}
 		</div>
